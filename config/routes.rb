ae5f4ab70b8ee4470a6cf990fd0e1a7cd4cf8fa6
John Kueh <me@yoongfook.com>
2013-05-25 14:22:42 +1000

    survey redirect link and added missing link on landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Binable::Application.routes.draw do
   get "pages/list"
   get "pages/leaderboard"
   get "pages/landing"
+  match "/survey" => redirect("https://binable.typeform.com/to/uYEj7w")
 
   root :to => 'pages#landing'
 end
